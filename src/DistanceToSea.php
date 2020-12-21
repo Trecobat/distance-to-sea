4681c36063674baf817ab3e46e9b451ed1c8002b
Andrey Ratnikov <a.ratnikov97@gmail.com>
2020-12-21 12:12:26 +0400

    fix name caspian sea

--- src/DistanceToSea.php
+++ src/DistanceToSea.php
@@ -16,9 +16,9 @@ class DistanceToSea
 {
     public const SEA_OF_AZOV = 'sea_of_azov';
     public const BLACK_SEA = 'black_sea';
-    public const CASPEAN_SEA = 'caspean_sea';
+    public const CASPIAN_SEA = 'caspian_sea';
     public const BALTIC_SEA = 'baltic_sea';
-    public const SEAS = [self::SEA_OF_AZOV, self::BLACK_SEA, self::CASPEAN_SEA, self::BALTIC_SEA];
+    public const SEAS = [self::SEA_OF_AZOV, self::BLACK_SEA, self::CASPIAN_SEA, self::BALTIC_SEA];
 
     /**
      * @var DistanceToSea|null
